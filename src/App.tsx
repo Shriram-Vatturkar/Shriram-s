4a0993fadca91709424d79bf5cc7ac03c5a53543
Shriram-Vatturkar <shriram.vatturkar@gmail.com>
2025-07-23 22:16:24 +0530

    vercel analytics component added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ProjectDetail from "./pages/ProjectDetail";
 import NotFound from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/react"; // Import for React applications
 
 const queryClient = new QueryClient();
 
@@ -22,6 +23,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Analytics /> {/* Add the Analytics component here */}
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
